1167218ab6a85a1f408437c6a22df2c453721d0b
Eshwar <142719089+3aswarconnect@users.noreply.github.com>
2024-07-22 21:26:57 +0530

    Update RiderForm.js

--- src/RiderForm.js
+++ src/RiderForm.js
@@ -38,45 +38,33 @@ function RiderForm() {
   return (
     <div className="rider-container">
       <div className="rider-form-container">
-        <h2>Rider Details</h2>
+        <h2>Rider Form</h2>
         <form onSubmit={handleSubmit}>
           <div className="form-field">
-            <label>
-              Bike:
-              <input type="text" name="bike" value={rideDetails.bike} onChange={handleChange} required />
-            </label>
+            <label htmlFor="bike">Bike:</label>
+            <input type="text" name="bike" value={rideDetails.bike} onChange={handleChange} required />
+          </div>
+          <div className="form-field date-container">
+            <label htmlFor="time">Time:</label>
+            <input type="date" name="time" value={rideDetails.time} onChange={handleChange} required />
           </div>
           <div className="form-field">
-            <label>
-              Time:
-              <input type="datetime-local" name="time" value={rideDetails.time} onChange={handleChange} required />
-            </label>
+            <label htmlFor="destination">Destination:</label>
+            <input type="text" name="destination" value={rideDetails.destination} onChange={handleChange} required />
           </div>
           <div className="form-field">
-            <label>
-              Destination:
-              <input type="text" name="destination" value={rideDetails.destination} onChange={handleChange} required />
-            </label>
+            <label htmlFor="fare">Fare:</label>
+            <input type="text" name="fare" value={rideDetails.fare} onChange={handleChange} required />
           </div>
           <div className="form-field">
-            <label>
-              Fare:
-              <input type="text" name="fare" value={rideDetails.fare} onChange={handleChange} required />
-            </label>
-          </div>
-          <div className="form-field">
-            <label>
-              Contact Number:
-              <input type="text" name="contactNumber" value={rideDetails.contactNumber} onChange={handleChange} required />
-            </label>
+            <label htmlFor="contactNumber">Contact Number:</label>
+            <input type="text" name="contactNumber" value={rideDetails.contactNumber} onChange={handleChange} required />
           </div>
           <button type="submit" className="save-button">Save</button>
+          <div className="form-footer">
+            <p>Share your ride details here to let others see the information easily.</p>
+          </div>
         </form>
-        <footer className="form-footer">
-          <p>
-            Enter your ride details here. This information will be visible to others and will help them connect with you for a smooth ride experience.
-          </p>
-        </footer>
       </div>
     </div>
   );
